a0520722978efc1a89842a7ba29da3e12eb8b31e
Roy97 <rd062595@gmail.com>
2024-10-24 18:07:53 +1100

    initial commit

---
+++ agent_llm.py
@@ -0,0 +1,99 @@
+import os
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_chroma import Chroma
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_unstructured import UnstructuredLoader
+from langchain_ai21 import AI21SemanticTextSplitter
+
+from langchain_community.vectorstores.utils import filter_complex_metadata
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.prebuilt import create_react_agent
+
+class AgentLLM:
+
+    def __init__(self):
+
+        os.environ["UNSTRUCTURED_API_KEY"] = "Ifx4J2RRTUaCYXALmRcoH1ucCr70Pd"
+        os.environ["AI21_API_KEY"] = "VXgSDh46P4UWXyTXpF0PNNedEYgl9rdZ"
+
+        self.agent=None
+        self.chat_history = []
+        self.agent_scratchpad = []
+    
+    def RAG(self, doc_path=None):
+        
+        llm = ChatOllama(model="llama3.2")
+        memory = MemorySaver()
+        if doc_path:
+            loader = UnstructuredLoader(doc_path)
+            docs = loader.load()
+            embeddings = OllamaEmbeddings(model="nomic-embed-text")
+            ai21_sts = AI21SemanticTextSplitter(chunk_size=10000, chunk_overlap=2000)
+            for i in docs:
+                if len(i.page_content) < 30:
+                    i.page_content = i.page_content + " " * (30 - len(i.page_content))
+            split_docs = ai21_sts.split_documents(docs)
+            filter_docs = filter_complex_metadata(split_docs, allowed_types=(str, bool, int, float))
+            vector_store = Chroma.from_documents(filter_docs, embeddings)
+            retriever = vector_store.as_retriever(search_type = "mmr", k=10)
+
+            system_prompt = [
+                ("system", "You are an assistant for question-answering tasks."
+                            "Answer the following questions as best you can."
+                            "You have access to tools (use them only when the question matches the tool description)."
+                            "Provide long answers with proper formatting and detailed analysis."
+                            "If you don't know the answer, just say that you don't know."
+                            ),
+                ("placeholder", "{messages}"),
+                ("placeholder", "{chat_history}"),
+                ("placeholder", "{agent_scratchpad}")
+            ]
+
+            prompt = ChatPromptTemplate.from_messages(system_prompt)
+            tool = create_retriever_tool(retriever, "document_retriever", "Searches, analyses, and returns relevant information from the document based on the user's question")
+            self.agent = create_react_agent(llm, [tool], state_modifier=prompt, checkpointer=memory)
+            #chain = ({"context": retriever | format_docs, "question": RunnablePassthrough()} | prompt | llm | StrOutputParser())
+        else:
+            system_prompt = [
+                ("system", "You are a helpful and friendly assistant for question-answering tasks."
+                            "Answer the following questions as best you can."
+                            "Provide detailed answers with proper formatting."
+                            "If you don't know the answer, just say that you don't know."
+                            ),
+                ("placeholder", "{messages}"),
+                ("placeholder", "{chat_history}"),
+                ("placeholder", "{agent_scratchpad}")
+            ]
+
+            prompt = ChatPromptTemplate.from_messages(system_prompt)
+            self.agent = create_react_agent(llm, [], state_modifier=prompt, checkpointer=memory)
+    
+    def format_docs(self, docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    def conversation(self, query):
+        #print("Enter query:")
+        #query = input()
+        #result = chain.invoke(query)
+        #print("\n", result)
+        human_message = None
+        ai_message = None
+        for event in self.agent.stream(
+            {"messages": [HumanMessage(content=query)], "chat_history": self.chat_history, "agent_scratchpad": self.agent_scratchpad},
+            config={"configurable": {"thread_id": "rag0"}},
+            stream_mode="values",
+            ):
+            for message in event["messages"]:
+                if isinstance(message, HumanMessage):
+                    human_message = message
+                elif isinstance(message, AIMessage):
+                    ai_message = message
+                elif isinstance(message, ToolMessage):
+                    self.agent_scratchpad.append(message)
+        self.chat_history.append(human_message)
+        self.chat_history.append(ai_message)
+        #human_message.pretty_print()
+        #ai_message.pretty_print()
+        return ai_message.content
---
+++ main.py
@@ -0,0 +1,199 @@
+import flet as ft
+from agent_llm import AgentLLM
+
+async def main(page: ft.Page):
+
+    def send_message(e):
+        query = chat_input.value
+        process(True)
+        if len(query) > 0:
+            human_chat_message_text = ft.Text(
+                expand=True,
+                text_align=ft.TextAlign.START,
+            
+            )
+            human_chat_message = ft.Row(
+                alignment=ft.MainAxisAlignment.START,
+                expand=True,
+                controls = [
+                    ft.CircleAvatar(
+                        content=ft.Text("U"),
+                    ),
+                    ft.ResponsiveRow(
+                        alignment=ft.MainAxisAlignment.START,
+                        expand=True,
+                        controls = [
+                            ft.Card(
+                                col = min(len(max(query.split("\n"), key=len))/10 + 0.5, 6),
+                                content = ft.Container(
+                                    expand=True,
+                                    alignment=ft.alignment.center_left,
+                                    padding=10,
+                                    content=human_chat_message_text
+                                )   
+                            )
+                        ]
+                    )
+                ]
+            )
+            human_chat_message_text.value = query
+            chat.controls.append(human_chat_message)
+            chat_input.value = ""
+            page.update()
+            response = llm_agent.conversation(query)
+            on_response(response)
+    
+    def on_response(response):
+        ai_chat_message_text = ft.Text(
+            expand=True,
+            value=response,
+            text_align=ft.TextAlign.START,
+        )
+        ai_chat_message = ft.Row(
+            alignment=ft.MainAxisAlignment.END,
+            expand=True,
+            controls = [
+                ft.ResponsiveRow(
+                    alignment=ft.MainAxisAlignment.END,
+                    expand=True,
+                    controls = [
+                        ft.Card(
+                            col = min(len(max(response.split("\n"), key=len))/10 + 0.5, 6),
+                            content = ft.Container(
+                                expand=True,
+                                alignment=ft.alignment.center_right,
+                                padding=10,
+                                content=ai_chat_message_text
+                            )
+                        )
+                    ]
+                ),
+                ft.CircleAvatar(
+                    content=ft.Text("AI"),
+                    )
+            ]
+        )
+        chat.controls.append(ai_chat_message)
+        process(False)
+        page.update()
+    
+    def get_file_path(e):
+        if e.files:
+            file_path = e.files[0].path.replace("\\", "/")
+            process(True)
+            page.update()
+            llm_agent.RAG(file_path)
+            update_filename(e.files[0].name)
+    
+    def update_filename(filename):
+        process(False)
+        upload_button.text = filename
+        page.update()
+    
+    def change_theme(e):
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+            e.control.icon = ft.cupertino_icons.MOON_FILL
+        elif page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+            e.control.icon = ft.cupertino_icons.SUN_MIN_FILL
+        page.update()
+    
+    def clear_chat(e):
+        chat.controls.clear()
+        page.update()
+    
+    def clear_upload(e):
+        llm_agent.RAG()
+        upload_button.text = "Upload file"
+        page.update()
+    
+    def process(hide):
+        if hide:
+            process_indicator.visible = True
+            chat_input.disabled = True
+            send_button.disabled = True
+            upload_button.disabled = True
+            clear_upload_button.disabled = True
+        else:
+            process_indicator.visible = False
+            chat_input.disabled = False
+            send_button.disabled = False
+            upload_button.disabled = False
+            clear_upload_button.disabled = False
+
+    page.appbar = ft.CupertinoAppBar(
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        padding=-10,
+        leading=ft.CupertinoButton(icon=ft.cupertino_icons.SUN_MIN_FILL, on_click=change_theme),
+        middle=ft.Text("Agent LLM"),
+        trailing=ft.CupertinoButton(icon=ft.cupertino_icons.REFRESH_BOLD, on_click=clear_chat)
+    )
+    chat = ft.ListView(
+        expand=True,
+        auto_scroll=True,
+        padding=20,
+        spacing=10,
+    )
+    chat_input = ft.CupertinoTextField(
+        col = {"xs": 6, "sm": 8, "md": 10},
+        height=75,
+        multiline=True,
+        shift_enter=True,
+        placeholder_text="Type your query",
+        on_submit=send_message
+    )
+    file_dialog = ft.FilePicker(on_result=get_file_path)
+    upload_button = ft.CupertinoButton(
+        icon=ft.cupertino_icons.UPLOAD_CIRCLE,
+        text="Upload file",
+        on_click=lambda _:file_dialog.pick_files(
+            allowed_extensions=["pdf"],
+            allow_multiple=False
+        )
+    )
+    clear_upload_button = ft.CupertinoButton(
+        icon = ft.cupertino_icons.CLEAR_THICK,
+        on_click=clear_upload
+    )
+    send_button = ft.CupertinoFilledButton(
+        col = {"xs": 6, "sm": 4, "md": 2},
+        text="Send",
+        on_click=send_message
+    )
+    input_area = ft.ResponsiveRow(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            chat_input,
+            ft.Row(
+                alignment=ft.MainAxisAlignment.CENTER,
+                controls=[
+                    upload_button,
+                    clear_upload_button,
+                ]
+            ),
+            send_button
+        ]
+    )
+    chat_area = ft.Container(
+        expand=True,
+        content = ft.Column(
+            expand=True,
+            scroll=ft.ScrollMode.ADAPTIVE,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls = [
+                chat
+            ]
+        )
+    )
+    process_indicator = ft.ProgressBar(visible=False)
+    llm_agent = AgentLLM()
+    llm_agent.RAG()
+    page.title = "AgentLLM"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.overlay.append(file_dialog)
+    page.add(process_indicator)
+    page.add(chat_area)
+    page.add(input_area)
+
+ft.app(port = 7000, target = main, route_url_strategy = "hash", assets_dir = "assets", view=ft.AppView.WEB_BROWSER)
